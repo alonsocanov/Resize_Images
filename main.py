3b0f82df48af3fefb8a555ac6aeb4d3b05e6d050
alonsocanov <alonsocanov@hotmail.com>
2021-03-07 14:35:50 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import glob
+import cv2
+import numpy as np
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Lane detection script')
+    parser.add_argument('path', type=str, help='Images directory')
+    parser.add_argument('factor', type=float, help='Reduce image factor')
+    parser.add_argument('--type', type=str, default='jpeg', help='Image type')
+    args = parser.parse_args()
+
+    files = None
+    if os.path.exists(args.path):
+        if os.path.isdir(args.path):
+            path = ''.join([args.path, '/*.', args.type])
+            files = glob.glob(path)
+        elif os.path.isfile(args.path):
+            files = [args.path]
+    else:
+        print('No Such file or directory')
+        sys.exit(1)
+    if not files:
+        print('Directory empty')
+        sys.exit(1)
+
+    for file in files:
+        img = cv2.imread(file)
+        h, w = img.shape[:2]
+        img_resize = cv2.resize(
+            img, (int(w * args.factor), int(h * args.factor)))
+        cv2.imwrite(file, img_resize)
+
+
+if __name__ == '__main__':
+    main()
